8826af672dbf78471a506401f3b1525a7e2db6c1
Steve Arkan Fitrano <stevelewiss2003@gmail.com>
2026-07-13 14:14:40 +0700

    Initial Commit

---
+++ C03A06/C03A06App.swift
@@ -0,0 +1,32 @@
+//
+//  C03A06App.swift
+//  C03A06
+//
+//  Created by Steve on 13/07/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct C03A06App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ C03A06Tests/C03A06Tests.swift
@@ -0,0 +1,19 @@
+//
+//  C03A06Tests.swift
+//  C03A06Tests
+//
+//  Created by Steve on 13/07/26.
+//
+
+import Testing
+@testable import C03A06
+
+struct C03A06Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
