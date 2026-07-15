0cff7e3ad8c09d691dedd38f56e7c338ea3c4f61
natgwk02 <nwidjaja01@student.ciputra.ac.id>
2026-07-15 22:07:12 +0700

    feat: Membuat reflection data model, mulai dari mendefinisikan entitas Reflection dan Question, menambahkan relasi follow-up question, dan membuat seed data untuk kumpulan pertanyaan harian yang akan digunakan pada fitur refleksi. (#TEC-205)

--- C03A06/Model/Moment.swift
+++ C03A06/Model/Moment.swift
@@ -5,3 +5,27 @@
 //  Created by Steve on 14/07/26.
 //
 
+import Foundation
+import SwiftData
+
+@Model
+final class Moment {
+    @Attribute(.externalStorage) var photo: Data
+    var timestamp: Date
+    var shortDescription: String?
+
+    @Relationship(deleteRule: .cascade, inverse: \Reflection.moment)
+    var reflection: Reflection?
+
+    init(
+        photo: Data,
+        timestamp: Date,
+        shortDescription: String? = nil,
+        reflection: Reflection? = nil
+    ) {
+        self.photo = photo
+        self.timestamp = timestamp
+        self.shortDescription = shortDescription
+        self.reflection = reflection
+    }
+}
