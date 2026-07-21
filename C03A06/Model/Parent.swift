a11f31bb581f3580a3be5a24d2168e0feb7dce24
Steve Arkan Fitrano <stevelewiss2003@gmail.com>
2026-07-21 16:43:28 +0700

    feat: defining parents model (#tec-246)

---
+++ C03A06/Model/Parent.swift
@@ -0,0 +1,26 @@
+//
+//  Parent.swift
+//  C03A06
+//
+//  Created by Steve on 21/07/26.
+//
+import Foundation
+import SwiftData
+
+
+enum ParentRole: String, Codable {
+    case ayah
+    case ibu
+}
+
+@Model
+class Parent {
+    var name: String
+    var parentRole: ParentRole
+    
+    init(name: String, parentRole: ParentRole) {
+        self.name = name
+        self.parentRole = parentRole
+    }
+}
+
