b68f030a91c9fda61e439e714d4bd98abc0fcd19
Deny Wahyudi Asaloei <115266792+denywa@users.noreply.github.com>
2026-07-26 03:40:17 +0700

    fix: warna pakai primary

---
+++ C03A06/Features/RecapMoment/ArchiveHighlight.swift
@@ -0,0 +1,8 @@
+//
+//  ArchiveHighlight.swift
+//  C03A06
+//
+//  Created by Deny Wahyudi Asaloei  on 26/07/26.
+//
+
+import Foundation
---
+++ C03A06/Features/RecapMoment/SuccessOverlay.swift
@@ -0,0 +1,18 @@
+//
+//  SuccessOverlay.swift
+//  C03A06
+//
+//  Created by Deny Wahyudi Asaloei  on 24/07/26.
+//
+
+import SwiftUI
+
+struct SuccessOverlay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuccessOverlay()
+}
